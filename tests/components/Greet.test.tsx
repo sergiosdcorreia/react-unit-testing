c4a5f71b085298d1aaf7b6c14e80a01c7d1f644b
Sérgio Correia <sergiosdcorreia@gmail.com>
2024-05-13 23:08:29 +0100

    Setting up global imports

--- tests/components/Greet.test.tsx
+++ tests/components/Greet.test.tsx
@@ -1,4 +1,3 @@
-import { it, expect, describe } from "vitest";
 import { render, screen } from "@testing-library/react";
 import Greet from "../../src/components/Greet";
 import "@testing-library/jest-dom/vitest";
